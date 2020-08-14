e2ff81025e9b5033eeda07260ae437c6d995d179
santer545 <obram22@gmail.com>
2020-08-14 18:36:18 +0300

    desktop version

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 const hamburger = document.getElementById('nav-icon2');
+const header = document.querySelector('.js-header-nav')
 
-if(hamburger) {
-    hamburger.addEventListener("click", function() {
-        this.classList.toggle("open")
+if (hamburger) {
+    hamburger.addEventListener("click", function () {
+        this.classList.toggle("open");
+        header.classList.toggle("active")
     })
 }
 
@@ -11,4 +13,17 @@ var slider = tns({
     items: 1,
     slideBy: 'page',
     autoplay: false
-  });
+});
+
+var sliderGallery = tns({
+    container: '.js-gallery',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    responsive: {
+        700: {
+            items: 3,
+            gutter: 30
+        },
+    }
+});
