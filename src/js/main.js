bed0c76474504b7451d18f4ddbec1969c1573d75
santer545 <obram22@gmail.com>
2020-08-17 23:36:24 +0300

    add modal

--- src/js/main.js
+++ src/js/main.js
@@ -27,3 +27,17 @@ var sliderGallery = tns({
         },
     }
 });
+
+$.fn.setCursorPosition = function (pos) {
+    if ($(this).get(0).setSelectionRange) {
+        $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+        var range = $(this).get(0).createTextRange();
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select();
+    }
+};
+
+$("[type='tel']").mask("(999) 999-9999");
